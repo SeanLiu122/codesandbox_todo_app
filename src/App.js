fb772d5478ad1ba5292a57a1aca4a93ee896ca16
Sean Liu <jingfengliu231@gmail.com>
2020-05-06 14:34:27 -0400

    Data Fetching with React Hooks
    Data fetching with react effect hook section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./styles.css";
 
 // From: https://hackernoon.com/how-to-take-advantage-of-local-storage-in-your-react-projects-a895f2b2d3f2
@@ -7,40 +8,38 @@ import InputItem from "./components/InputItem";
 import DisplayField from "./components/DisplayField";
 
 export default function App() {
-  const [entry, setEntry] = useState("");
+  const [data, setData] = useState({ hits: [] });
   const [todo, setTodo] = useState([]);
 
-  // componentWillUnMount() {
-  //   this.addItem();
-  // }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=redux"
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
-  handleInputItem = (key, value) => {
+  const handleInputItem = (key, value) => {
     // create new item
     const nItem = {
       id: key + Math.floor(Math.random() * 100),
       value: value
     };
-
     // copy current list of items
-    const list = [...this.state.todos];
-
+    const list = [...todo];
     // add the new item to the list
     list.push(nItem);
-
     // update state with new list, reset the input
-    this.setState({
-      todos: list,
-      newItem: ""
-    });
-
-    localStorage.setItem("todos", this.state.todos);
+    setTodo(list);
+    localStorage.setItem("todos", JSON.stringify(todo));
   };
 
-  deleteItem = id => {
-    this.setState({
-      todos: this.state.todos.filter(item => item.id !== id)
-    });
-  };
+  function deleteItem(id) {
+    const filter = todo.filter(item => item.id !== id);
+    setTodo(filter);
+  }
 
   return (
     <div className="App">
@@ -53,13 +52,17 @@ export default function App() {
         }}
       >
         <h1>To-Do App</h1>
-        <InputItem
-          newItem={this.state.newItem}
-          handleInputItem={this.handleInputItem}
-        />
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href="item.url">{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <InputItem handleInputItem={handleInputItem} />
         <br /> <br />
         DisplayField:
-        <DisplayField list={this.state.todos} deleteItem={this.deleteItem} />
+        <DisplayField list={todo} deleteItem={deleteItem} />
       </div>
     </div>
   );
--- src/components/InputItem.js
+++ src/components/InputItem.js
@@ -1,22 +1,29 @@
 import React from "react";
 
 export default function inputItem(props) {
-  let { newItem, handleInputItem } = props;
+  let { handleInputItem } = props;
+  let entry = "";
 
   function handleChange(e) {
-    newItem = e.target.value;
+    entry = e.target.value;
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    handleInputItem(e.target.value, newItem);
+    handleInputItem(e.target.value, entry);
+    entry = "";
   }
 
   return (
     <React.Fragment>
       Add an item to the list:
       <br />
-      <input type="text" placeholder="Type item here" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="Type item here"
+        onChange={handleChange}
+        required
+      />
       <button onClick={handleSubmit}>&#43; Add</button>
     </React.Fragment>
   );
